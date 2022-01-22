48ce2c42f33b44a19b8d4519c7bf7051b7cc18d9
Zhian Mai <zmai@ucsd.edu>
2022-01-21 23:37:58 -0800

    upper bound finished

--- src/johnston/generic/wildcard/super_bound/Main.java
+++ src/johnston/generic/wildcard/super_bound/Main.java
@@ -10,13 +10,18 @@ import java.util.List;
 public class Main {
   public static void main(String[] args) {
     List<? super Shape> listA = new ArrayList<>();
-    listA.add(new Square(1));
-    listA.add(new Rectangle(2));
+    addShape(listA, new Square(1));
+    addShape(listA, new Rectangle(2));
     printShape(listA);
+  }
+
+  public static void addShape(List<? super Shape> list, Shape a) {
+    list.add(a);
   }
 
   public static void printShape(List<? super Shape> list) {
     for (int i = 0; i < list.size(); i++) {
+      // Cannot call list.get(i).draw(), they're all cast to Object type
       System.out.println(list.get(i).toString());
     }
   }
